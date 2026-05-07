9f54e1ad0539e1fe769bd239671ee0af281b0723
Juan Felipe Valencia Granada <rygbon64@gmail.com>
2026-05-07 17:53:52 -0500

    fix errors

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,6 +1,6 @@
 const usuarios = [
-    { nick: "Kuvshinov", password: 1234, img: "assets/image/kuvshinov-profile.jpg", frontpage: "assets/image/kuvshinov-portada.jpg" },
-    { nick: "samdoesarts", password: 12345, img: "assets/image/samdoesarts-profile.jpg", frontpage: "assets/image/samdoesart-portada.jpg" },
+    { nick: "Kuvshinov", password: 1234, img: "./assets/image/kuvshinov-profile.jpg", frontpage: "./assets/image/kuvshinov-portada.jpg" },
+    { nick: "samdoesarts", password: 12345, img: "./assets/image/samdoesarts-profile.jpg", frontpage: "./assets/image/samdoesart-portada.jpg" },
     { nick: "Philip", password: 123456, img: null }
 ]
 
